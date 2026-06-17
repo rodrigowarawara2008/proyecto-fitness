74bcd8aaee05e423b0964cfe397c8535fe24ea75
rodrigowarawara2008 <rodrigowarawara2008@cesanrafael.org>
2026-06-17 15:33:19 -0400

    se adasda

--- routes/carrito.py
+++ routes/carrito.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, session, redirect
+from flask import Blueprint, session, redirect, render_template
 import mysql.connector
 from config import Config
 
@@ -37,4 +37,64 @@ def agregar_carrito(producto_id):
     cursor.close()
     conexion.close()
 
-    return redirect('/inicio')
+    return redirect('/carrito')
+
+
+@carrito_bp.route('/carrito')
+def ver_carrito():
+
+    if 'usuario_id' not in session:
+        return redirect('/login')
+
+    conexion = conectar()
+
+    cursor = conexion.cursor(dictionary=True)
+
+    cursor.execute("""
+        SELECT
+            carrito.id,
+            productos.nombre,
+            productos.precio,
+            carrito.cantidad,
+            (productos.precio * carrito.cantidad) AS subtotal
+        FROM carrito
+        INNER JOIN productos
+            ON carrito.producto_id = productos.id
+        WHERE carrito.usuario_id = %s
+    """, (session['usuario_id'],))
+
+    productos = cursor.fetchall()
+
+    total = sum(
+        float(p['subtotal'])
+        for p in productos
+    )
+
+    cursor.close()
+    conexion.close()
+
+    return render_template(
+        'carrito.html',
+        productos=productos,
+        total=total
+    )
+
+
+@carrito_bp.route('/eliminar_carrito/<int:id>')
+def eliminar_carrito(id):
+
+    conexion = conectar()
+
+    cursor = conexion.cursor()
+
+    cursor.execute(
+        "DELETE FROM carrito WHERE id=%s",
+        (id,)
+    )
+
+    conexion.commit()
+
+    cursor.close()
+    conexion.close()
+
+    return redirect('/carrito')
