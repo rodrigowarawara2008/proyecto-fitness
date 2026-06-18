cda69132081b0b9adbbb30f08e5b60e434ba36b4
rodrigowarawara2008 <rodrigowarawara2008@cesanrafael.org>
2026-06-18 13:40:21 -0400

    adsasd

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect
 from config import Config
 
 from routes.usuarios import usuarios_bp
@@ -8,18 +8,17 @@ from routes.carrito import carrito_bp
 app = Flask(__name__)
 
 app.config.from_object(Config)
-
 app.secret_key = Config.SECRET_KEY
 
+# Blueprints
 app.register_blueprint(usuarios_bp)
 app.register_blueprint(productos_bp)
 app.register_blueprint(carrito_bp)
 
-
-
+# Al entrar a la web va directo a la tienda
 @app.route('/')
 def index():
-    return render_template('inicio.html')
+    return redirect('/inicio')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
